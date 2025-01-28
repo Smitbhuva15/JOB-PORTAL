82debb902bb08298721b2d1dee6ac6d178d791d6
Smitbhuva15 <itssmit123@gmail.com>
2025-01-28 21:38:55 +0530

    new password feature added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,7 +9,7 @@ const { getDataUri } = require("../utils/datauri");
 exports.signup = async (req, res) => {
     try {
         const { fullname, email, phoneNumber, password, role } = req.body;
-
+   
         
         if (!fullname || !email || !phoneNumber || !password || !role) {
             return res.status(400).json({
